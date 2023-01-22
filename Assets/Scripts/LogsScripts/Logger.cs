6c85910807e2b804097a3c5ecfbd5c47d8dbba81
Raphaël Pietrzak <raphael.pietrzak@etu.univ-cotedazur.fr>
2023-01-23 00:01:25 +0100

    fixed logger

--- Assets/Scripts/LogsScripts/Logger.cs
+++ Assets/Scripts/LogsScripts/Logger.cs
@@ -1,9 +1,13 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using SocketIOClient;
 using System.Threading;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System;
 
 public class Logger : MonoBehaviour
 {
@@ -19,10 +23,8 @@ public class Logger : MonoBehaviour
     {
         _clientObject = GameObject.Find("SocketIOClient");
         _initClient = _clientObject.GetComponent<InitiatlisationClient>();
+        StartCoroutine(loadLogsFromServer());
 
-        // dataObject = GameObject.Find("DataContainer");
-        // _data = dataObject.GetComponent<Datas>();
-        // Debug.Log("start attack manager "+_data.placedNpcList);
         var thread = new Thread(SocketThread);
         thread.Start();
 
@@ -49,37 +51,55 @@ public class Logger : MonoBehaviour
     {
         while (_client == null)
         {
-
-            _clientObject = GameObject.Find("SocketIOClient");
-            _initClient = _clientObject.GetComponent<InitiatlisationClient>();
             _client = _initClient.client;
             Thread.Sleep(500);
         }
-        // while (_data == null)
-        // {
-        //     Debug.Log("Data null attack manager");
-        //     dataObject = GameObject.Find("DataContainer");
-        //     _data = dataObject.GetComponent<Datas>();
-        //     Thread.Sleep(300);
-        // }
 
         _client.On("log", (data) =>
         {
             _initClient._mainThreadhActions.Enqueue(() =>
             {
-                LogInfo logInfo = JsonUtility.FromJson<LogInfo>(data.GetValue(0).ToString());
-                Debug.Log("Logging " + logInfo.logText);
-
-                GameObject logPanel = Instantiate(logPanelPrefab);
-                Transform logScrollView = gameObject.transform.Find("GlobalPanel").Find("LogsPanel").Find("ScrollView");
-                logPanel.transform.SetParent(logScrollView.Find("Viewport").Find("Content"));
-
-                fillLogPanel(logInfo, logPanel.transform);
-                
-                logScrollView.GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
+                if (SceneManager.GetActiveScene().name == "Logs")
+                {
+                    LogInfo logInfo = JsonUtility.FromJson<LogInfo>(data.GetValue(0).ToString());
+                    createLogPanel(logInfo);
+                }
             });
         });
 
+    }
+
+    IEnumerator loadLogsFromServer() {
+        var www = UnityWebRequest.Get(_initClient.requestURI+"/mjLogs");
+    
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success) {
+            Debug.Log(www.error);
+        }
+        else {
+            // Show results as text
+            var jsonResponse = www.downloadHandler.text;
+            var logInfos = JsonUtility.FromJson<LogList>(jsonResponse);
+
+            foreach (LogInfo logInfo in logInfos.listOfLogs)
+            {
+                createLogPanel(logInfo);
+            }
+        }
+    }
+
+    private void createLogPanel(LogInfo logInfo)
+    {
+        Debug.Log("Logging " + logInfo.logText);
+
+        GameObject logPanel = Instantiate(logPanelPrefab);
+        Transform logScrollView = gameObject.transform.Find("GlobalPanel").Find("LogsPanel").Find("ScrollView");
+        logPanel.transform.SetParent(logScrollView.Find("Viewport").Find("Content"));
+
+        fillLogPanel(logInfo, logPanel.transform);
+        
+        logScrollView.GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
     }
 
     private void fillLogPanel(LogInfo log, Transform logPanelTransform)
@@ -98,9 +118,16 @@ public class Logger : MonoBehaviour
     }
 }
 
+[Serializable]
 public class LogInfo
 {
     public string imagePath;
     public string title;
     public string logText;
+}
+
+[Serializable]
+public class LogList
+{
+    public List<LogInfo> listOfLogs;
 }
